67fee1d48e418699e9e0dc86b401e4a2abe1eef1
Abror Abdusalomov <corporationsystems7@gmail.com>
2021-08-17 00:11:54 +0500

    16.08.2021 - converted to react

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from "react";
 import "./App.css";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Menu from "./components/Menu/Menu";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 
 const App = (props) => {
   return (
     <BrowserRouter>
-        <Menu />
+      <Menu />
+      <Switch>
         <Route exact path="/portfolio" render={() => <Portfolio />} />
         <Route exact path="/" render={() => <Main />} />
-        <Footer />
+      </Switch>
+      <Footer />
     </BrowserRouter>
   );
 };
